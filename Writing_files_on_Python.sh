efc1dc27841118e3e1c8ceca1837f3cb29a08962
Zilentjd <47437935+LeonPV-J@users.noreply.github.com>
2021-03-28 19:54:25 -0500

    Add a bash script file

---
+++ Writing_files_on_Python.sh
@@ -0,0 +1,11 @@
+[julianmoreno@zerpent ~]$ python3
+Python 3.9.1 (default, Dec  8 2020, 00:00:00) 
+[GCC 10.2.1 20201125 (Red Hat 10.2.1-9)] on linux
+Type "help", "copyright", "credits" or "license" for more information.
+>>> with open('novel.txt', 'w') as file:
+...     file.write("It was a dark and stormy night")
+... 
+30
+>>> exit()
+[julianmoreno@zerpent ~]$ 
+
