38560dbc68bcd79edc72021ee6102a63a1a3dd43
My name <me@example.com>
2021-03-31 14:40:21 -0500

    Rename min_absolute ro min_gb, use parameter names

---
+++ all_checks.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import sys
+
+def check_reboot():
+    """Returns True if the computer has a pending reboot."""
+    return os.path.exists("/run/reboot-required")
+
+def check_disk_full(disk, min_gb, min_percent):
+    """Returns True if there isn't enough disk space, False otherwise."""
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    # Calculate how many free gygabytes
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gygabytes_free < min_gb:
+        return False
+    return True
+
+def main():
+    if check_reboot():
+        print("Pending Reboot.")
+        sys.exit(1)
+    if check_disk_full(disk="/", min_gb=2, min_percent=10):
+        print("disk full.")
+        sys.exit(1)
+
+    print("Everything ok.")
+    sys.exit(0)
+
+main()
