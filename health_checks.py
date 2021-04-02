3e5b42290d7d64d7feb8cc463bb37045df3e4c74
My name <me@example.com>
2021-04-02 01:51:30 -0500

    Add a simple network connectivity check
    Integrated CPU check as well

--- health_checks.py
+++ health_checks.py
@@ -29,13 +29,21 @@ def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise """
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
 
 def main():
     checks = [
         (check_reboot, "Pending Reboot."),
         (check_root_full, "Root partition full."),
         (cpu_constrained, "CPU load too high."),
-    ]
+        (check_no_network, "No working network."),
+        ]
     everything_ok = True
     for check, msg in checks:
         if check():
