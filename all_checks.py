7ff9f1e78012d5ca793553580947a02297cf6e89
My name <me@example.com>
2021-04-02 01:18:47 -0500

    Iterate over a list of checks and messages to avoid code duplication

--- all_checks.py
+++ all_checks.py
@@ -18,17 +18,22 @@ def check_disk_full(disk, min_gb, min_percent):
     if percent_free < min_percent or gigabytes_free < min_gb:
         return True
     return False
+
+
 def check_root_full():
     """Returns True if the root partition is full, False otherwise."""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
+
 def main():
-    if check_reboot():
-        print("Pending Reboot.")
-        sys.exit(1)
-    if check_root_full():
-        print("Root partition full.")
-        sys.exit(1)
+    checks=[
+        (check_reboot, "Pending Reboot."),
+        (check_root_full, "Root partition full.")
+    ]
+    for check, msg in checks:
+        if check():
+            print(msg)
+            sys.exit(1)
 
     print("Everything ok.")
     sys.exit(0)
